34957b2c684a908232030adc055cb2f9c0af7fbd
Sara <131993370+Smoe111@users.noreply.github.com>
2025-04-13 00:35:37 -0500

    Clase Docente

--- Leer Docentes/Estructura.exs
+++ Leer Docentes/Estructura.exs
@@ -1,8 +1,24 @@
 defmodule Estructura do
+
   def main do
 
     "Docentes_Uniquindio_20250413.csv"
     |> Docente.leer_csv()
     |> filtrar_datos()
+    |> Docente.generar_mensaje_docente()
+    |> Util.mostrarMensaje()
   end
+
+  defp generar_mensaje(docente) do
+    "#{docente.periodo}, #{docente.formacion}, #{docente.vinculacion}"
+  end
+
+  defp filtrar_datos(datos) do
+   datos
+   |>Enum.filter(fn(docente)-> docente.formacion == "MAESTRIA" and docente.vinculacion == "PLANTA" end)
+  end
+
+
 end
+
+Estructura.main()
